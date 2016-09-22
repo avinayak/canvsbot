078c65330263ac9f168e90f69935a17b8a30b0a8
Atul Vinayak <su.atul.vi@gmail.com>
2016-09-22 19:01:24 +0530

    upp

--- bb.py
+++ bb.py
@@ -3,16 +3,17 @@ from PIL import ImageDraw
 import random
 import numpy as np
 import bezier
+import time
 
-N_CURVES = 100
-N_COLORS = 10
-MAX_THICKNESS = 3
+N_CURVES = 50
+N_COLORS = 3
+MAX_THICKNESS = 2
 WIDTH = 1000
 HEIGHT = 1000
-BORDER_FRAC = 1
+BORDER_FRAC = 2
 MAX_POPULATION = 40
-N_CHILD = 10
-MUTATION_FACTOR = 0.1
+N_CHILD = MAX_POPULATION/2
+MUTATION_FACTOR = 0.01
 
 def draw_bezier(xyz,thick,col,draw):
     ts = [t/100.0 for t in range(101)]
@@ -77,30 +78,36 @@ class CandidateImage():
     def get_fitness(self,truth):
         np_candidate = np.asarray(self.get_image())
         np_truth = np.asarray(truth)
-        return 1-(np.sum(np.absolute(np_truth-np_candidate))/(WIDTH*HEIGHT*255.0))
+        return (np.sum(np.absolute(np_truth-np_candidate))/(WIDTH*HEIGHT*255.0))
 
     def __repr__(self):
         return "<%2.17f>"%self.fitness
 
 if __name__ == '__main__':
+    p=0
     truth = Image.open('relativity.jpg').convert('L')
     best_image = CandidateImage()
     WIDTH,HEIGHT = truth.size
     pool = []
     for _ in range(MAX_POPULATION):
         pool.append(CandidateImage().random(truth))
+    print pool
     while True:
+        pool.sort(key=lambda x:x.fitness, reverse=True)
         for i in range(N_CHILD):
             sum,dartboard = monte_carlo_dartboard(pool)
             c1 = pool[try_darts(sum, dartboard)]
             c2 = pool[try_darts(sum, dartboard)]
             ch = cross(c1,c2,truth)
             pool[-i]=ch
-        pool.sort(key=lambda x:x.fitness, reverse=True)
         if pool[0].fitness > best_image.fitness:
             best_image = pool[0]
-            print best_image
-            best_image.get_image().save(repr(best_image.fitness)+".png")
+            print best_image,time.asctime()
+            best_image.get_image().save(repr(best_image.fitness)[2:10]+".png")
+            if p<4:
+                p+=1
+            else:
+                exit()
     #fitnesses = list(map((lambda x:x.get_fitness(truth)),pool))
     # print pool
     # cim.save('out.png')
